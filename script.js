1d1689ec784878d217a66dd7784b05933e772f7f
Leonardo Garcia <leonardoggarcia48@unoeste.edu.br>
2023-05-22 14:06:58 -0300

    Projeto: Ajustado nome das funcoes

--- script.js
+++ script.js
@@ -17,9 +17,30 @@ function controlAudio(id, option){
    
    switch(option){
      case action.play : 
+        let lista = document.getElementsByClassName("fa-pause")
+
+        for(let item of lista){
+            item.classList.remove("fa-pause"); 
+            item.classList.add("fa-play")
+        }
+
         element.firstElementChild.classList.remove("fa-play"); 
         element.firstElementChild.classList.add("fa-pause"); 
         playingSound = !playingSound;
+
+        if(id == 1){
+            audio.src = "sounds/break.mp3"
+        }
+        else if(id == 2){
+            audio.src = "sounds/futuristic-beat-146661.mp3"
+        }
+        else if(id == 3){
+            audio.src = "sounds/modern-vlog-140795.mp3"
+        }
+        else if(id == 4){
+            audio.src = "sounds/relaxing-145038.mp3"
+        }
+
         audio.play()
      break;
      case action.pause: 
@@ -32,7 +53,7 @@ function controlAudio(id, option){
   
 }
 
-function controlVolume(id, option){
+function controlMuted(id, option){
     const element = document.getElementById(id);
 
     option = muteSound ? action.high : option;
@@ -54,4 +75,7 @@ function controlVolume(id, option){
     
  }
  
+// function controlVoume(){
+// audio.volume = 1
+// }
 
